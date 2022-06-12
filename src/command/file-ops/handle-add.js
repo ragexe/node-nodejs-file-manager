92dc96dacb8da724267af924d08a14a3b5404ac5
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-13 01:36:31 +0400

    feat: implement `add`

--- src/command/file-ops/handle-add.js
+++ src/command/file-ops/handle-add.js
@@ -1,3 +1,25 @@
-export const handleAdd = () => {
-  console.log("handle-add works");
+import { writeFile } from "node:fs/promises";
+
+import { validatePath } from "../../utils/validate-path.js";
+
+const create = async (filePath, fileContent = "") => {
+  try {
+    await writeFile(filePath, fileContent, { flag: "ax" });
+
+    return Promise.resolve(void 0);
+  } catch (error) {
+    if (error && error.code === "EEXIST") {
+      throw new Error("FS operation failed");
+    }
+
+    throw error;
+  }
+};
+
+export const handleAdd = async ({ path }, input) => {
+  const [_, targetPath] = input.split("add ");
+
+  const validatedTargetPath = validatePath(targetPath, { path });
+
+  return create(validatedTargetPath);
 };

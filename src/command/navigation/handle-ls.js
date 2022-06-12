fd94326ac6234c816ab5aca17e9592d405108718
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-13 00:28:36 +0400

    feat: implement `ls`

--- src/command/navigation/handle-ls.js
+++ src/command/navigation/handle-ls.js
@@ -1,3 +1,23 @@
-export const handleLs = () => {
-  console.log(`handle ls works`);
+import { readdir } from "node:fs/promises";
+
+const list = async (sourceFolderPath) => {
+  try {
+    const files = await readdir(sourceFolderPath, { withFileTypes: true });
+
+    files.forEach(({ name: fileName }) => {
+      console.log("\x1b[36m%s\x1b[0m", fileName);
+    });
+
+    return Promise.resolve(void 0);
+  } catch (error) {
+    if (error && error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+
+    throw error;
+  }
+};
+
+export const handleLs = async ({ path }) => {
+  await list(path.current);
 };
--- src/operators/command-operator.js
+++ src/operators/command-operator.js
@@ -1,27 +1,27 @@
-import { Transform } from 'stream';
+import { Transform } from "stream";
 
-import { handleAdd } from '../command/file-ops/handle-add.js';
-import { handleCat } from '../command/file-ops/handle-cat.js';
-import { handleCp } from '../command/file-ops/handle-cp.js';
-import { handleMv } from '../command/file-ops/handle-mv.js';
-import { handleRm } from '../command/file-ops/handle-rm.js';
-import { handleRn } from '../command/file-ops/handle-rn.js';
-import { handleDefault, handleError } from '../command/handle-default.js';
-import { handleExit } from '../command/handle-exit.js';
-import { handleHash } from '../command/handle-hash.js';
-import { handleCd } from '../command/navigation/handle-cd.js';
-import { handleLs } from '../command/navigation/handle-ls.js';
-import { handleOsArchitecture } from '../command/os/handle-os-architecture.js';
-import { handleOsCpus } from '../command/os/handle-os-cpus.js';
-import { handleOsEol } from '../command/os/handle-os-eol.js';
-import { handleOsHomedir } from '../command/os/handle-os-homedir.js';
-import { handleOsUsername } from '../command/os/handle-os-username.js';
-import { handleCompress } from '../command/zip/handle-compress.js';
-import { handleDecompress } from '../command/zip/handle-decompress.js';
+import { handleAdd } from "../command/file-ops/handle-add.js";
+import { handleCat } from "../command/file-ops/handle-cat.js";
+import { handleCp } from "../command/file-ops/handle-cp.js";
+import { handleMv } from "../command/file-ops/handle-mv.js";
+import { handleRm } from "../command/file-ops/handle-rm.js";
+import { handleRn } from "../command/file-ops/handle-rn.js";
+import { handleDefault, handleError } from "../command/handle-default.js";
+import { handleExit } from "../command/handle-exit.js";
+import { handleHash } from "../command/handle-hash.js";
+import { handleCd } from "../command/navigation/handle-cd.js";
+import { handleLs } from "../command/navigation/handle-ls.js";
+import { handleOsArchitecture } from "../command/os/handle-os-architecture.js";
+import { handleOsCpus } from "../command/os/handle-os-cpus.js";
+import { handleOsEol } from "../command/os/handle-os-eol.js";
+import { handleOsHomedir } from "../command/os/handle-os-homedir.js";
+import { handleOsUsername } from "../command/os/handle-os-username.js";
+import { handleCompress } from "../command/zip/handle-compress.js";
+import { handleDecompress } from "../command/zip/handle-decompress.js";
 
 export const getCommandOperator = (store) =>
   new Transform({
-    transform: (chunk, _, callback) => {
+    transform: async (chunk, _, callback) => {
       const input = chunk.toString().trim().toLowerCase();
 
       try {
@@ -31,13 +31,13 @@ export const getCommandOperator = (store) =>
             return;
 
           case input === "up":
-            handleCd(store, 'cd ./..');
+            handleCd(store, "cd ./..");
             break;
           case input.startsWith("cd"):
             handleCd(store, input);
             break;
           case input === "ls":
-            handleLs(store);
+            await handleLs(store);
             break;
 
           case input.startsWith("cat"):

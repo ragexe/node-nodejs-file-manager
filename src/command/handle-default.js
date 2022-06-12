194bc3564a259397714f004aaa9645d72ee2588a
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-13 00:28:19 +0400

    feat: pretty output

--- src/command/handle-default.js
+++ src/command/handle-default.js
@@ -1,7 +1,7 @@
-export const handleDefault = (message) => {
-  console.log("Invalid input");
+export const handleDefault = () => {
+  console.log("\x1b[33m%s\x1b[0m", "Invalid input");
 };
 
-export const handleError = (_) => {
-  console.log("Operation failed");
+export const handleError = (error) => {
+  console.log("\x1b[31m%s\x1b[0m", "Operation failed", error && error.message);
 };
--- src/operators/logger-operator.js
+++ src/operators/logger-operator.js
@@ -3,7 +3,12 @@ import { Transform } from "stream";
 export const getLoggerOperator = ({ path }) =>
   new Transform({
     transform: (_, __, callback) => {
-      console.log(`You are currently in ${path.current}`);
+      console.log(
+        "\x1b[2m%s\x1b[34m%s\x1b[0m",
+        `You are currently in ${path.current} `,
+        path.current === path.root ? "(home)" : ""
+      );
+
       callback(null, _);
     },
   });

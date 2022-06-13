04e65e698df57f961fb26263c0c2307d3a32e6bc
Raman Makeichyk <raman.makeichyk@customertimes.com>
2022-06-13 07:44:25 +0400

    refactor: rework `add` with Stream API

--- src/command/file-ops/handle-add.js
+++ src/command/file-ops/handle-add.js
@@ -1,25 +1,37 @@
-import { writeFile } from "node:fs/promises";
+import { createWriteStream } from "fs";
+import { pipeline } from "node:stream/promises";
+import { Readable } from "stream";
 
 import { validatePath } from "../../utils/validate-path.js";
 
-const create = async (filePath, fileContent = "") => {
-  try {
-    await writeFile(filePath, fileContent, { flag: "ax" });
+export const write = async (targetFilePath, fileContent = "") => {
+  return new Promise((resolve, reject) => {
+    const readableStream = Readable.from(fileContent);
+    const writableStream = createWriteStream(targetFilePath, { flags: "ax" });
 
-    return Promise.resolve(void 0);
-  } catch (error) {
-    if (error && error.code === "EEXIST") {
-      throw new Error("FS operation failed");
-    }
+    return pipeline(readableStream, writableStream)
+      .then(() => {
+        resolve(void 0);
+      })
+      .catch((error) => reject(error));
+  });
 
-    throw error;
-  }
+  return promise;
 };
 
 export const handleAdd = async ({ path }, input) => {
-  const [_, targetPath] = input.split("add ");
+  const [_, commandParams] = input.split("add ");
+
+  if (!commandParams) throw new Error("No provided arguments for create file");
+
+  const argNames = commandParams
+    .replaceAll("'", '"')
+    .match(/(?:[^\s"]+|"[^"]*")+/g)
+    .map((arg) => arg.replaceAll('"', "").trim());
+
+  const [targetPath, fileContent = ""] = argNames;
 
   const validatedTargetPath = validatePath(targetPath, { path });
 
-  return create(validatedTargetPath);
+  return write(validatedTargetPath, fileContent);
 };
